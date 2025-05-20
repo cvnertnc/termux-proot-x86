c1d6e532d93213a4aa7368bcefcd7160ddfa4f87
cvnertnc <the.civanertunc@gmail.com>
2025-05-20 08:59:47 +0300

    Setup: Added Cleaner all Installed

--- setup.sh
+++ setup.sh
@@ -1,43 +1,63 @@
 #!/bin/bash
 
-# Choice of architecture
-echo "Select your architecture:"
-echo "1) amd64 / x86_64"
-echo "2) i686 / x86"
-echo "3) Both (amd64 and i686)"
-printf "Enter your choice (1, 2 or 3): "
-read choice < /dev/tty  # stdin'i açıkça tty'den oku
+echo "Select an option:"
+echo "1) Install amd64 / x86_64"
+echo "2) Install i686 / x86"
+echo "3) Install both (amd64 and i686)"
+echo "4) Uninstall everything and clean up"
+printf "Enter your choice (1, 2, 3 or 4): "
+read choice < /dev/tty
 
-# Upgrade
-echo "Updating packages..."
-apt update -y && apt upgrade -y
-
-# Common dependencies
-apt install git blink proot-distro dos2unix -y
-
-# Clone repo (skip if you already have one)
-if [ ! -d "termux-proot-x86" ]; then
-    git clone https://github.com/cvnertnc/termux-proot-x86
+# Update packages and install common dependencies unless cleaning
+if [[ "$choice" != "4" ]]; then
+    echo "Updating packages..."
+    apt update -y && apt upgrade -y
+    apt install git blink proot-distro dos2unix -y
 fi
-cd termux-proot-x86 || exit 1
 
-# AMD64 Installation
+# Function: Install amd64 support
 setup_amd64() {
-    echo "Installing for amd64 / x86_64..."
+    echo "Installing support for amd64 / x86_64..."
     apt install qemu-user-x86-64 -y
     cp ./distros-x86_64/* $PREFIX/etc/proot-distro
     dos2unix $PREFIX/etc/proot-distro/*-x86_64.sh > /dev/null
 }
 
-# i686 installation
+# Function: Install i686 support
 setup_i686() {
-    echo "Installing for i686 / x86..."
+    echo "Installing support for i686 / x86..."
     apt install qemu-user-i386 -y
     cp ./distros-i686/* $PREFIX/etc/proot-distro
     dos2unix $PREFIX/etc/proot-distro/*-i686.sh > /dev/null
 }
 
-# Operation by selection
+# Function: Full cleanup
+cleanup_all() {
+    echo "Cleaning up all files and packages..."
+
+    # Remove cloned repo
+    rm -rf ~/termux-proot-x86
+
+    # Remove installed PRoot-distro scripts
+    rm -f $PREFIX/etc/proot-distro/*-x86_64.sh
+    rm -f $PREFIX/etc/proot-distro/*-i686.sh
+
+    # Optional: Remove installed packages
+    pkg uninstall qemu-user-x86-64 qemu-user-i386 blink proot-distro git dos2unix -y
+
+    echo "Cleanup complete."
+    exit 0
+}
+
+# Clone repo if needed
+if [[ "$choice" =~ [1-3] ]]; then
+    if [ ! -d "termux-proot-x86" ]; then
+        git clone https://github.com/cvnertnc/termux-proot-x86
+    fi
+    cd termux-proot-x86 || exit 1
+fi
+
+# Perform operation based on user choice
 case "$choice" in
     1) setup_amd64 ;;
     2) setup_i686 ;;
@@ -45,6 +65,7 @@ case "$choice" in
         setup_amd64
         setup_i686
         ;;
+    4) cleanup_all ;;
     *)
         echo "Invalid choice. Exiting."
         exit 1
@@ -52,4 +73,4 @@ case "$choice" in
 esac
 
 echo "Done. You can now run: proot-distro list"
-echo "Then install with: proot-distro install <distro>"
+echo "Then install a distro using: proot-distro install <distro-name>"
